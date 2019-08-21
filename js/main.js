452e6d190a4c570d85683147608204f1227c48d8
gluehq <tim@glue.co.nz>
2019-08-21 16:01:31 +1200

    Diminishing skew options

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ var check_media;
 // Make sure these correspond with CSS
 
 var phone_limit = 414;
-var plus_limit = 737;
+var plus_limit = 668;
 var tablet_limit = 835;
 var compact_limit = 1025;
 var standard_limit = 1367;
